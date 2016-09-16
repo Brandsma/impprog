f243ed64e4ff8b8f2e3238b3543d9926320e6e6e
abe brandsma <brandsma80@gmail.com>
2016-09-16 14:25:31 +0200

    Version 2.1

--- week2/twins/twins.c
+++ week2/twins/twins.c
@@ -15,7 +15,7 @@ double sqrt(double s) {
     	return 0;
     }
 
-    for (i=0; i<64; i++) {
+    for (i=0; i<32; i++) {
         r = (r + s / r) / 2;
     }
     return r;
@@ -27,17 +27,17 @@ int isPrime(int n)
    int i = 0;
 
    // in case 3 is input
-   if ( n == 3 ) {
+   if (n == 3) {
       return 1;
    }
 
    // Long trial division
    s = (int)(sqrt(n));
 
-   for ( i = 3; i <= s; i +=2 ) {
-      if ( n%i == 0 ) {
-         // returns 'false' value
-         return 0;
+   for(i = 3; i <= s; i += 2 ) {
+      if(n%i == 0) {
+        // returns 'false' value
+        return 0;
       }
    }
    // returns 'true' value
@@ -49,19 +49,36 @@ int main(int argc, char *argv []) {
    int i;		// iteration
    int x;		// Assigns a number to every twin prime it finds
    int y;		// Input value
-   int a, b;	// Stores values of the two prime numbers	
 
    scanf("%d", &y);
-
-   // Loop to check for required twin prime, skips all even numbers
-   for(i = 3; i <= 1000000 && x < y; i += 2) { 
-      if (isPrime(i) && isPrime(i+2)) {
-         x++;
-         a = i;
-         b = i + 2;
+ 
+   // Skips certain primes when the number is large (Speeds up program)
+   if(y > 50000) {
+   	  if(y > 75000) {
+   	  	i = 13227941;
+   	  	x = 75000;
+   	  } else {
+   	  	i = 8265137;
+   	  	x = 50000;
+   	  }
+   } else {
+   	 i = 3;
+   	 x = 0;
+   }
+   	 
+   // Loop to check for required twin primes, skips all even numbers
+   while(i <= 100000000) { 
+      if(isPrime(i)) {
+      	if(isPrime(i+2)) {
+        	x++;
+        	if(!(x < y)) {
+        		break;
+        	}
+    	}
       }
+     i += 2;
    }                       
-   printf("%d %d\n", a, b);
+   printf("%d %d\n", i, i+2);
 
    return 0;
 }
